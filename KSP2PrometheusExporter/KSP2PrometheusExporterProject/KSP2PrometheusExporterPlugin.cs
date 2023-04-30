0422a147b514a694f7523b407c14cee231464c6e
Goddchen <goddchen@gmail.com>
2023-04-30 17:13:50 +0200

    feat(exporter): add prometheus settings and push endpoint config options
    Added a new configuration entry to allow changing the port where the metrics will be served under /metrics. Also added a new configuration entry to allow defining the push endpoint for sending metrics to a Prometheus push gateway. If no value is provided, metrics will not be pushed anywhere.
    
    Disclaimer: This message has been generated with gpt-commit-messages: https://github.com/Goddchen/gpt-commit-messages
    Signed-off-by: Goddchen <goddchen@gmail.com>

--- KSP2PrometheusExporter/KSP2PrometheusExporterProject/KSP2PrometheusExporterPlugin.cs
+++ KSP2PrometheusExporter/KSP2PrometheusExporterProject/KSP2PrometheusExporterPlugin.cs
@@ -1,16 +1,10 @@
 ﻿using BepInEx;
-using HarmonyLib;
-using KSP.UI.Binding;
 using SpaceWarp;
-using SpaceWarp.API.Assets;
 using SpaceWarp.API.Mods;
 using SpaceWarp.API.Game;
-using SpaceWarp.API.Game.Extensions;
-using SpaceWarp.API.UI;
-using SpaceWarp.API.UI.Appbar;
-using UnityEngine;
 using Prometheus;
 using KSP.Sim.impl;
+using BepInEx.Configuration;
 
 namespace KSP2PrometheusExporter;
 
@@ -18,17 +12,6 @@ namespace KSP2PrometheusExporter;
 [BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
 public class KSP2PrometheusExporterPlugin : BaseSpaceWarpPlugin
 {
-    // These are useful in case some other mod wants to add a dependency to this one
-    public const string ModGuid = MyPluginInfo.PLUGIN_GUID;
-    public const string ModName = MyPluginInfo.PLUGIN_NAME;
-    public const string ModVer = MyPluginInfo.PLUGIN_VERSION;
-
-    private bool _isWindowOpen;
-    private Rect _windowRect;
-
-    private const string ToolbarFlightButtonID = "BTN-KSP2PrometheusExporterFlight";
-    private const string ToolbarOABButtonID = "BTN-KSP2PrometheusExporterOAB";
-
     public static KSP2PrometheusExporterPlugin Instance { get; set; }
 
     private static readonly Prometheus.Gauge ActiveVesselVehicleAltitudeFromSeaLevel = Metrics.CreateGauge(
@@ -86,6 +69,19 @@ public class KSP2PrometheusExporterPlugin : BaseSpaceWarpPlugin
 
         Instance = this;
 
+        ConfigEntry<int> portConfigEntry = Config.Bind<int>("Prometheus Settings",
+            "port",
+            9102,
+            new BepInEx.Configuration.ConfigDescription(
+                "Port where the metrics will be served under /metrics (defaults to 9102)")
+            );
+        ConfigEntry<String> pushEndpointConfigEntry = Config.Bind<string>("Prometheus Settings",
+            "push-engpoint",
+            null,
+            new ConfigDescription("The address of the Prometheus push gateway to send metrics to (defaults to none)")
+            );
+
+
         Metrics.DefaultRegistry.AddBeforeCollectCallback(() =>
         {
             VesselVehicle activeVesselVehicle = Vehicle.ActiveVesselVehicle;
@@ -126,94 +122,20 @@ public class KSP2PrometheusExporterPlugin : BaseSpaceWarpPlugin
 
         Metrics.SuppressDefaultMetrics();
 
-        new MetricServer(9102).Start();
+        new MetricServer(portConfigEntry.Value).Start();
 
         Logger.LogInfo("Started Prometheus exporter on port 9102");
 
-        new MetricPusher(new MetricPusherOptions
+        if (pushEndpointConfigEntry.Value != null)
         {
-            Endpoint = "http://51.15.51.177:9091/metrics",
-            Job = "ksp2",
-            ReplaceOnPush = true,
-        }).Start();
+            new MetricPusher(new MetricPusherOptions
+            {
+                Endpoint = pushEndpointConfigEntry.Value,
+                Job = "ksp2",
+                ReplaceOnPush = true,
+            }).Start();
 
-        Logger.LogInfo("Started Prometheus metrics pusher to push metrics to http://51.15.51.177:9091/metrics");
-
-        //// Register Flight AppBar button
-        //Appbar.RegisterAppButton(
-        //    "KSP2 Prometheus Exporter",
-        //    ToolbarFlightButtonID,
-        //    AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
-        //    isOpen =>
-        //    {
-        //        _isWindowOpen = isOpen;
-        //        GameObject.Find(ToolbarFlightButtonID)?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(isOpen);
-        //    }
-        //);
-
-        //// Register OAB AppBar Button
-        //Appbar.RegisterOABAppButton(
-        //    "KSP2 Prometheus Exporter",
-        //    ToolbarOABButtonID,
-        //    AssetManager.GetAsset<Texture2D>($"{SpaceWarpMetadata.ModID}/images/icon.png"),
-        //    isOpen =>
-        //    {
-        //        _isWindowOpen = isOpen;
-        //        GameObject.Find(ToolbarOABButtonID)?.GetComponent<UIValue_WriteBool_Toggle>()?.SetValue(isOpen);
-        //    }
-        //);
-
-        //// Register all Harmony patches in the project
-        //Harmony.CreateAndPatchAll(typeof(KSP2PrometheusExporterPlugin).Assembly);
-
-        //// Try to get the currently active vessel, set its throttle to 100% and toggle on the landing gear
-        //try
-        //{
-        //    var currentVessel = Vehicle.ActiveVesselVehicle;
-        //    if (currentVessel != null)
-        //    {
-        //        currentVessel.SetMainThrottle(1.0f);
-        //        currentVessel.SetGearState(true);
-        //    }
-        //}
-        //catch (Exception) { }
-
-        //// Fetch a configuration value or create a default one if it does not exist
-        //var defaultValue = "my_value";
-        //var configValue = Config.Bind<string>("Settings section", "Option 1", defaultValue, "Option description");
-
-        //// Log the config value into <KSP2 Root>/BepInEx/LogOutput.log
-        //Logger.LogInfo($"Option 1: {configValue.Value}");
+            Logger.LogInfo($"Started Prometheus metrics pusher to push metrics to {pushEndpointConfigEntry.Value}");
+        }
     }
-
-    ///// <summary>
-    ///// Draws a simple UI window when <code>this._isWindowOpen</code> is set to <code>true</code>.
-    ///// </summary>
-    //private void OnGUI()
-    //{
-    //    // Set the UI
-    //    GUI.skin = Skins.ConsoleSkin;
-
-    //    if (_isWindowOpen)
-    //    {
-    //        _windowRect = GUILayout.Window(
-    //            GUIUtility.GetControlID(FocusType.Passive),
-    //            _windowRect,
-    //            FillWindow,
-    //            "KSP2 Prometheus Exporter",
-    //            GUILayout.Height(350),
-    //            GUILayout.Width(350)
-    //        );
-    //    }
-    //}
-
-    ///// <summary>
-    ///// Defines the content of the UI window drawn in the <code>OnGui</code> method.
-    ///// </summary>
-    ///// <param name="windowID"></param>
-    //private static void FillWindow(int windowID)
-    //{
-    //    GUILayout.Label("KSP2 Prometheus Exporter - Prometheus exporter for telemetry data.");
-    //    GUI.DragWindow(new Rect(0, 0, 10000, 500));
-    //}
 }
